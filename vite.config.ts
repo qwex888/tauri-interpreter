00ddcd117933df6a4ecfbf698eebeac74f8cea6b
游猫 <qwex333@126.com>
2024-03-27 22:54:34 +0800

    readme

--- vite.config.ts
+++ vite.config.ts
@@ -18,4 +18,6 @@ export default defineConfig(async () => ({
       ignored: ["**/src-tauri/**"],
     },
   },
+   // to access the Tauri environment variables set by the CLI with information about the current target
+   envPrefix: ['VITE_', 'TAURI_PLATFORM', 'TAURI_ARCH', 'TAURI_FAMILY', 'TAURI_PLATFORM_VERSION', 'TAURI_PLATFORM_TYPE', 'TAURI_DEBUG'],
 }));
