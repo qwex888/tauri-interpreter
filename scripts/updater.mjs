6fd1169e21f2e0c3e2d9a9f5c069ca978b034661
qwex888 <qwex333@126.com>
2024-07-17 15:03:09 +0800

    update updater

--- scripts/updater.mjs
+++ scripts/updater.mjs
@@ -1,41 +1,60 @@
-import { context, getOctokit } from "@actions/github";
-import { readFile } from "node:fs/promises";
+import { context, getOctokit } from '@actions/github';
+import { readFile } from 'node:fs/promises';
 
 const octokit = getOctokit(process.env.GITHUB_TOKEN);
 
+const tagName = 'updater';
 const updateRelease = async () => {
-  // 获取updater tag的release
-  const { data: release } = await octokit.rest.repos.getReleaseByTag({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    tag: "updater",
-  });
-  // 删除旧的的文件
-  const deletePromises = release.assets
-    .filter((item) => item.name === "latest.json")
-    .map(async (item) => {
-      await octokit.rest.repos.deleteReleaseAsset({
+  let release = null;
+  console.log('------>actions:context: ', context);
+  try {
+    // 获取updater tag的release
+    const { data } = await octokit.rest.repos.getReleaseByTag({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      tag: tagName
+    });
+    release = data
+    // 删除旧的的文件
+    const deletePromises = release.assets
+      .filter(item => item.name === 'latest.json')
+      .map(async item => {
+        await octokit.rest.repos.deleteReleaseAsset({
+          owner: context.repo.owner,
+          repo: context.repo.repo,
+          asset_id: item.id
+        });
+      });
+
+    await Promise.all(deletePromises);
+  } catch (error) {
+    if (error.status === 404 || error.message === 'release not found') {
+      const createdRelease = await octokit.rest.repos.createRelease({
         owner: context.repo.owner,
         repo: context.repo.repo,
-        asset_id: item.id,
+        tag_name: tagName,
+        name: 'Tauri Interpreter',
+        body: '',
+        draft: true,
+        prerelease: false,
+        target_commitish: context.sha
       });
-    });
-
-  await Promise.all(deletePromises);
-
+      release = createdRelease.data;
+    }
+  }
   // 上传新的文件
-  const file = await readFile("latest.json", { encoding: "utf-8" });
+  const file = await readFile('latest.json', { encoding: 'utf-8' });
   const data = JSON.parse(file);
-  if (data.platforms["darwin-x86_64"]) {
-    data.platforms["darwin-aarch64"] = data.platforms["darwin-x86_64"];
+  if (data.platforms['darwin-x86_64']) {
+    data.platforms['darwin-aarch64'] = data.platforms['darwin-x86_64'];
   }
 
   await octokit.rest.repos.uploadReleaseAsset({
     owner: context.repo.owner,
     repo: context.repo.repo,
     release_id: release.id,
-    name: "latest.json",
-    data: JSON.stringify(data, null, 2),
+    name: 'latest.json',
+    data: JSON.stringify(data, null, 2)
   });
 };
 
