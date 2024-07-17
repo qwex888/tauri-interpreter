25719e7554be838f4e69bd342c320372415e296c
qwex888 <qwex333@126.com>
2024-07-17 13:44:16 +0800

    fix error

--- scripts/publish.mjs
+++ scripts/publish.mjs
@@ -1,13 +1,22 @@
 import { readFile, copyFile, writeFile, mkdir } from "node:fs/promises";
 import { join } from "node:path";
 
+// const artifactPaths = JSON.parse(process.env.artifactPaths);
+// console.log("artifactPaths", artifactPaths);
+// console.log("first", artifactPaths[0]);
 const targetDir = join(import.meta.dirname, "../", "publish");
 
 const publish = async () => {
   await mkdir(targetDir, { recursive: true });
 
+  // for await (const artifactPath of artifactPaths) {
+  //   const fileName = artifactPath.split("/").pop();
+  //   const path = join(targetDir, fileName);
+  //   await copyFile(artifactPath, path);
+  //   console.log(`Coping ${artifactPath} ======> ${path}`);
+  // }
+
   const file = await readFile("latest.json", { encoding: "utf-8" });
-  // 这里我是需要手动处理一下json 所以先读取在写入到指定目录，具体到逻辑可以完全按照您自己的想法来
   const data = JSON.parse(file);
   if (data.platforms["darwin-x86_64"]) {
     data.platforms["darwin-aarch64"] = data.platforms["darwin-x86_64"];
@@ -19,4 +28,3 @@ const publish = async () => {
 };
 
 publish();
-
--- scripts/updater.mjs
+++ scripts/updater.mjs
@@ -1,8 +1,6 @@
-// 注意要安装@actions/github依赖
 import { context, getOctokit } from "@actions/github";
 import { readFile } from "node:fs/promises";
 
-// 在容器中可以通过env环境变量来获取参数
 const octokit = getOctokit(process.env.GITHUB_TOKEN);
 
 const updateRelease = async () => {
@@ -27,13 +25,17 @@ const updateRelease = async () => {
 
   // 上传新的文件
   const file = await readFile("latest.json", { encoding: "utf-8" });
+  const data = JSON.parse(file);
+  if (data.platforms["darwin-x86_64"]) {
+    data.platforms["darwin-aarch64"] = data.platforms["darwin-x86_64"];
+  }
 
   await octokit.rest.repos.uploadReleaseAsset({
     owner: context.repo.owner,
     repo: context.repo.repo,
     release_id: release.id,
     name: "latest.json",
-    data: file,
+    data: JSON.stringify(data, null, 2),
   });
 };
 
